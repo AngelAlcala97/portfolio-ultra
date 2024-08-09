6f4580f4f82e07a83b552212d49af5166b2de3c6
Angel Alcala <angel_alcala_@outlook.com>
2024-08-09 15:38:01 +0200

    c

--- js/projects.js
+++ js/projects.js
@@ -30,11 +30,11 @@
                     <a target="_blank" href="${ANGEL.projects[i].enlace}">
                     <div class="cardImage" style="background-image: url(${ANGEL.projects[i].imageURL});">
                         <div class="slider">
-                            <img src="img/estrellas-slider1.webp">
-                            <img src="img/estrellas-slider1.webp">
-                            <img src="img/estrellas-slider1.webp">
-                            <img src="img/estrellas-slider1.webp">
-                            <img src="img/estrellas-slider1.webp">
+                            <img src="img/projects/estrellas-slider1.webp">
+                            <img src="img/projects/estrellas-slider1.webp">
+                            <img src="img/projects/estrellas-slider1.webp">
+                            <img src="img/projects/estrellas-slider1.webp">
+                            <img src="img/projects/estrellas-slider1.webp">
                         </div>
                     </div>
                     <div class="cardText">
